a0cdd8c6a8169d856009dca0f008782316c62b01
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-19 11:18:51 +0200

    Removed: Unecessary imports <> JS

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import express from "express";
 import { exec } from "child_process";
 import util from "util";
 import fs from "fs";
-const app = express();
-const PORT = 4000;
+
 const execPromise = util.promisify(exec);
-app.use(express.json());
 
 async function extractAudio(videoPath, outputDir) {
     const audioPath = `${outputDir}/audio.mp3`;
